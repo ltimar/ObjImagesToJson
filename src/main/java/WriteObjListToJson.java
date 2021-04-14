5a2ec030e0ad53c0b37286f6568afde872ee632a
Andrei Timar <atimar999@gmail.com>
2021-04-14 09:13:38 +0300

    initial commit

--- src/main/java/WriteObjListToJson.java
+++ src/main/java/WriteObjListToJson.java
@@ -6,27 +6,18 @@ import java.io.FileWriter;
 import java.nio.file.Paths;
 
 public class WriteObjListToJson {
-    //public final static String  jsonFileName = "d:\\Lori\\2021\\proiecte\\AndreiOBJToJson\\MulteOb.json";
-    public final static String  jsonFileName = "d:\\Lori\\2021\\proiecte\\AndreiOBJToJson\\SimpleRobotRez.json";
 
-    //public void creareObjectMapper(){
-    public static void main(String args[]){
-        String objFileName = "d:\\Lori\\2021\\proiecte\\AndreiOBJToJson\\SimpleRobot.obj";
-       // String objFileName = "d:\\Lori\\2021\\proiecte\\AndreiOBJToJson\\3D_Arrows_by_Peter_Iliev_obj.obj";
-       // String objFileName = args[0];
-       // String jsonFileName = args[1];
-
-        ObjListFromObjInputFile lauObjList = new ObjListFromObjInputFile(objFileName);
+    public static void main(String args[]) {
+        ObjListFromObjInputFile lauObjList = new ObjListFromObjInputFile(args[0]);
 
         // create object mapper instance
         ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
 
-        try( BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(jsonFileName))) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(args[1]))) {
             // convert map to JSON file
             // sa verific cum arata lista
-            mapper.writeValue(Paths.get(jsonFileName).toFile(), lauObjList.creareObjectListForJson());
-        }
-        catch (Exception ex) {
+            mapper.writeValue(Paths.get(args[1]).toFile(), lauObjList.creareObjectListForJson());
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
